624c7ffb143c8a11ac4e614915f75d33eded1ace
Angel Alberto Dauzon Martinez <angel_barsa05@hotmail.com>
2020-07-12 01:03:58 -0500

    Se agrega carpeta docs

---
+++ docs/precache-manifest.ce7978fd483556d0547975156261b562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a342e76f89bbd5ebf77e360621dea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4b9734aac5b300f4f8",
+    "url": "/static/css/main.9a6fbbe8.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4b9734aac5b300f4f8",
+    "url": "/static/js/main.3c6269ef.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
